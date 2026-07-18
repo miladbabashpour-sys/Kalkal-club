4e3dc8f3277553b75fd07e0245e96e64430ae213
miladbabashpour-sys <miladbabashpour@gmail.com>
2026-07-18 23:36:20 +0330

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "اینجا API URL خودت";
+const SUPABASE_KEY = "اینجا Publishable key خودت";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
